da8c7febd87c0c049af853dbebc1a8690a44dbeb
Oleksandr Shtykhno <yukan@ukr.net>
2018-04-07 16:47:04 +0200

    Finish swagger annotations

--- server/src/main/scala/pcs/server/route/CataloguesRoute.scala
+++ server/src/main/scala/pcs/server/route/CataloguesRoute.scala
@@ -2,7 +2,7 @@ package pcs.server.route
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{PathMatcher, Route}
-import io.swagger.annotations.{Api, ApiOperation, ApiResponse, ApiResponses}
+import io.swagger.annotations._
 import javax.ws.rs.Path
 import pcs.server.JsonSupport
 import pcs.server.service.CataloguesService
@@ -13,7 +13,7 @@ import scala.concurrent.ExecutionContext
 /**
   * Defines REST for products in catalogues
   */
-@Api(value = "products")
+@Api(value = "products", produces = "application/json")
 @Path("/v1/products")
 class CataloguesRoute(cataloguesService: CataloguesService)
                      (implicit executionContext: ExecutionContext)
@@ -24,7 +24,11 @@ class CataloguesRoute(cataloguesService: CataloguesService)
   override lazy val routes: Route = find() ~ all ~ create ~ update ~ remove
   override lazy val path: PathMatcher[Unit] = "v1" / "products"
 
-  @ApiOperation(httpMethod = "GET", value = "Find product by ID")
+  @ApiOperation(httpMethod = "GET", value = "Find product", response = classOf[Product])
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "id", required = true, dataType = "integer", paramType = "path", value = "Product ID")
+  ))
+  @Path("/{id}")
   def find(): Route = {
     get {
       pathPrefix(path / LongNumber) { id =>
@@ -37,8 +41,11 @@ class CataloguesRoute(cataloguesService: CataloguesService)
     }
   }
 
-  @ApiOperation(httpMethod = "GET", value = "Find products")
-  @Path("/all")
+  @ApiOperation(httpMethod = "GET", value = "List products")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "limit", required = true, dataType = "integer", paramType = "path", value = "Limit")
+  ))
+  @Path("/all/{limit}")
   def all: Route = {
     get {
       pathPrefix(path / "all" / IntNumber) { limit =>
@@ -51,7 +58,10 @@ class CataloguesRoute(cataloguesService: CataloguesService)
     }
   }
 
-  @ApiOperation(httpMethod = "POST", value = "Create product")
+  @ApiOperation(httpMethod = "POST", value = "Create product", response = classOf[Product])
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "title", required = true, dataType = "string", paramType = "query", value = "Title")
+  ))
   def create: Route = {
     post {
       pathPrefix(path)
@@ -63,7 +73,10 @@ class CataloguesRoute(cataloguesService: CataloguesService)
     }
   }
 
-  @ApiOperation(httpMethod = "PUT", value = "Update product")
+  @ApiOperation(httpMethod = "PUT", value = "Update product", response = classOf[Product])
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "product", dataTypeClass = classOf[Product], required = true, paramType = "body", value = "Product")
+  ))
   def update: Route = {
     put {
       pathPrefix(path)
@@ -75,7 +88,10 @@ class CataloguesRoute(cataloguesService: CataloguesService)
     }
   }
 
-  @ApiOperation(httpMethod = "DELETE", value = "Delete product")
+  @ApiOperation(httpMethod = "DELETE", value = "Delete product", response = classOf[Product])
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "id", required = true, dataType = "integer", paramType = "path", value = "Product ID")
+  ))
   def remove: Route = {
     delete {
       pathPrefix(path / LongNumber) { id =>
